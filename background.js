160c21fb18a48cf6c9427d7f64f67e61a529eac8
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2024-06-12 19:13:52 +1000

    Automatically apply to new tabs

--- background.js
+++ background.js
@@ -1,3 +1,39 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed');
+    injectScriptToAllTabs();
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url) {
+        injectScript(tabId);
+    }
+});
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+    injectScript(details.tabId);
+}, {url: [{schemes: ['http', 'https']}]});
+
+chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === 'local' && changes.cursorHiderEnabled) {
+        injectScriptToAllTabs();
+    }
+});
+
+function injectScript(tabId) {
+    chrome.scripting.executeScript({
+        target: {tabId: tabId},
+        files: ['content.js']
+    }, () => {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        }
+    });
+}
+
+function injectScriptToAllTabs() {
+    chrome.tabs.query({}, (tabs) => {
+        for (let tab of tabs) {
+            injectScript(tab.id);
+        }
+    });
+}

651af43ba3a8f4eaff4487491a17afe15fc78ca4
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2024-06-13 20:11:58 +1000

    Add polling for update

--- content.js
+++ content.js
@@ -31,6 +31,34 @@ chrome.storage.local.get(['cursorHiderEnabled'], (result) => {
     }
 });
 
+// force update with poll
+
+setInterval(() => {
+    chrome.storage.local.get(['cursorHiderEnabled'], (result) => {
+
+        // don't update if it's the same
+        if (result.cursorHiderEnabled === window.cursorHiderEnabled) {
+            return;
+        }
+
+        window.cursorHiderEnabled = result.cursorHiderEnabled;
+
+        // always show and clear timeout just to reset it so we can at least see the mouse once
+
+        clearTimeout(window.hideCursorTimeout);
+
+        showCursor();
+
+
+        if (result.cursorHiderEnabled) {
+            document.addEventListener('mousemove', window.handleMouseMove);
+        } else {
+            document.removeEventListener('mousemove', window.handleMouseMove);
+        }
+    });
+}
+, 1000);
+
 window.hideCursor = hideCursor;
 window.showCursor = showCursor;
 window.handleMouseMove = handleMouseMove;
